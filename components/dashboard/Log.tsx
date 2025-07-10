af5e8b63307993ef990f9fddb6f665e021369c20
Vipul Thakur <vipulthakurofficial@gmail.com>
2025-07-11 02:46:01 +0530

    added audit trail

--- components/dashboard/Log.tsx
+++ components/dashboard/Log.tsx
@@ -2,8 +2,8 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
-export default function Log() {
-
+export default function Log({trail}: {trail: Array<Record<string, string | number | Date>>}) {
+    console.log("trail in Log", trail);
     const { status } = useSession();
     const { replace } = useRouter();
     useEffect(() => {
@@ -16,10 +16,19 @@ export default function Log() {
         <>
             <section className="ml-[210px] pt-5">
                 <h1>
-                    Hello from LogPage
+                    Audit Trail
                 </h1>
+                <hr className="my-3" />
+                {
+                    trail.map((item) => {
+                        return (
+                            <h3 key={Number(item.id)}
+                            className="p-2 bg-slate-200 w-fit rounded-lg mb-3"
+                            >{String(item.operation)}</h3>
+                        )
+                    })
+                }
             </section>
-           
         </>
     )
 }
--- pages/dashboard/log.tsx
+++ pages/dashboard/log.tsx
@@ -1,12 +1,39 @@
 import Log from "@/components/dashboard/Log"
 import SideMenu from "@/components/dashboard/SideMenu"
 import { SessionProvider } from "next-auth/react"
+import { PrismaClient } from "@/generated/prisma";
 
-export default function LogPage() {
+const prisma = new PrismaClient();
+
+export default function LogPage({trail}: {trail: Array<Record<string, string | number | Date>>}) {
     return (
         <SessionProvider>
             <SideMenu />
-            <Log />
+            <Log trail={trail} />
         </SessionProvider>
     )
+}
+
+
+export async function getServerSideProps() {
+  try {
+      const result = await prisma.auditTrail.findMany({orderBy: {createdAt: 'desc'}})
+      const serializedResult = result.map(item => ({
+        ...item,
+        createdAt: item.createdAt.toISOString()
+      }))
+      return {
+        props: {
+          trail: serializedResult,
+        }
+      }
+  } catch (error) {
+    console.log("error", error);
+    return {
+      props: {
+        trail: [],
+      }
+    }
+  }
+
 }
